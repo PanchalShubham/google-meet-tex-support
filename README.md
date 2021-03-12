f4014c635eac26b39fc5b59051180f6d33191d6f
Shubham Panchal <shubhampanchal9773@gmail.com>
2021-03-12 14:06:51 +0530

    allow enable/disable and parse nested expressions

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 This is a Greasemonkey/Tampermonkey user script which can be used to parse tex-equations in chat-window!
 
 # How to use  
-1. Install [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (if you are using Firefox) or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (if you are using Chrome).
-2. Add `script.js` as a new script to Greasemonkey/Tampermonkey.
-3. Browse https://meet.google.com and start writing an equation within $...$.
+1. Install [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (if you are using Firefox) or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (if you are using Chrome)
+2. Add `script.js` as a new script to Greasemonkey/Tampermonkey
+3. Browse https://meet.google.com
+4. Write markdown or inline equations using $..$ or block equations using $$..$$
 
 
 # Previews
 ![preview1](./preview/preview1.png)  
-
+![preview2](./preview/preview2.png)  
 
 # Suggestions?
 Please open an issue or code it up and send a pull request.  
--- script.js
+++ script.js
@@ -25,11 +25,13 @@ SOFTWARE.
 // ==UserScript==
 // @name         Google Meet Tex Support
 // @namespace    https://meet.google.com/
-// @version      0.1
+// @version      0.2
 // @description  "Adds Tex parsing for Google Meet"
 // @author       Shubham Panchal (shubhampanchal9773@gmail.com)
 // @match        https://meet.google.com/*
-// @require      https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.js
+// @require      https://cdn.jsdelivr.net/npm/markdown-it/dist/markdown-it.min.js
+// @require      https://cdn.jsdelivr.net/npm/katex/dist/katex.min.js
+// @require      https://cdn.jsdelivr.net/npm/markdown-it-texmath/texmath.min.js
 // ==/UserScript==
 
 // logging the startup of script
@@ -42,32 +44,119 @@ function addTexParser() {
     // check if head is available
     if (!head)  return;
     // add the CSS for beautiful display
-    head.innerHTML += `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.css" integrity="sha384-b1jwlKx2fZPr3OngW6CqA3niWl98suSWQIspVVyrImbCVlqAN4FS0K3kMQxWrFTy" crossorigin="anonymous">`;
+    head.innerHTML += `
+    <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/markdown-it-texmath/css/texmath.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.css" integrity="sha384-b1jwlKx2fZPr3OngW6CqA3niWl98suSWQIspVVyrImbCVlqAN4FS0K3kMQxWrFTy" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css"/>    <style>
+        .jtgSgd{
+            display: flex;
+            align-items: center;
+        }
+        .oIy2qc > p{
+            margin: 0px !important;
+            display: inline-block !important;
+        }
+        .tex_parser_toggler_container{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 5px;
+            background: #f1f3f4;
+        }
+        #tex_parser_toggler {
+          position: relative;
+          appearance: none;
+          outline: none;
+          width: 40px;
+          height: 18px;
+          border-radius: 10px;
+          background: gray;
+          box-shadow: none;
+          cursor: pointer;
+          overflow: hidden;
+          transition: 0.8s;
+        }
+        #tex_parser_toggler:checked {
+          background: #66aaa2;
+        }
+        #tex_parser_toggler::after {
+          content: '';
+          position: absolute;
+          border-radius: inherit;
+          width: 18px;
+          height: 18px;
+          background: #ffffff;
+          transition: inherit;
+        }
+        #tex_parser_toggler:checked::after {
+          left: calc(100% - 18px);
+          background: #00796b;
+        }
+    </style>
+    `;
+    const tm = texmath.use(katex);
+    const md = markdownit().use(tm, {
+        throwOnError: true,
+        engine: katex,
+        delimiters:'dollars',
+        katexOptions: {
+            macros: {"\\RR": "\\mathbb{R}"}
+        },
+    });
+
+    // parses the text
+    function parseText(item) {
+        let text = item.innerText;
+        let html = md.render(text);
+        item.innerHTML = html;
+    }
+
     // wait until the chat window is opened
     let interval = setInterval(()=>{
+        const messageClass = `oIy2qc`;
         // get the message container
-        let div = document.querySelector(`div[jsname="o4MlPd"]`);
+        let messageDiv = document.querySelector(`div[jsname="xySENc"]`);
         // check if message container is available
-        if (!div)  return;
+        if (!messageDiv)  return;
+        // get the item to place checkbox
+        let checkDiv = document.querySelector(`div[jsname="x1nnfb"]`);
+        // check if item is available
+        if (!checkDiv)  return;
+        // create input element
+        let checkbox = document.createElement(`input`);
+        checkbox.setAttribute(`type`, `checkbox`);
+        checkbox.setAttribute(`id`, `tex_parser_toggler`);
+        let span = document.createElement(`span`);
+        span.innerText = "Parse Tex";
+        span.setAttribute(`style`, `font-size: 18px;`)
+        let container = document.createElement(`div`);
+        container.classList.add(`tex_parser_toggler_container`);
+        container.appendChild(checkbox);
+        container.appendChild(span);
+        checkDiv.appendChild(container);
+
+
         // parses the equations into latex
-        function eventHandler(){
-            // get all the messages
-            let items = document.querySelectorAll(`div[class="oIy2qc"]`);
-            // iterate through messages
-            for (let i = 0; i < items.length; ++i){
-                // get the message text
-                let text = items[i].innerText;
-                // check if this is of the $...$
-                if (text.startsWith('$') && text.endsWith('$')){
-                    // parse the equation using KaTex
-                    let html = katex.renderToString(text.substring(1, text.length - 1), {throwOnError: false});
-                    // update the message to display the equation
-                    if (html)   items[i].innerHTML = html;
-                }
+        function eventHandler(event){
+            // check if parsing is enabled
+            if (!checkbox.checked)   return;
+            // get the target
+            let target = event.target;
+            let classList = target.classList;
+            // check if target's classList contains oIy2qc
+            if (classList.length === 1
+                    && classList[0] === messageClass) {
+                parseText(target);
+            } else {
+                // query the children with this class then
+                let items = target.querySelectorAll(`div[class="${messageClass}"]`);
+                // iterate through messages
+                for (const item of items) parseText(item);
             }
         };
         // update message whenever a new message is received
-        div.addEventListener(`DOMNodeInserted`, eventHandler);
+        messageDiv.addEventListener(`DOMNodeInserted`, eventHandler);
         // stop waiting for chat window!
         clearInterval(interval);
     }, 1000);
